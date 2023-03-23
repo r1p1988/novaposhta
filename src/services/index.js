8ed19595f8c6e3987583db15f8c38b4292244cdf
FARTUSHNYI OLEKSII <fartushnylex69@gmail.com>
2023-03-23 15:30:45 +0200

    change services

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 export const API = `https://api.novaposhta.ua/v2.0/json/`;
-
+export const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const controller = async (url, method = `GET`, obj) => {
   let options = {
