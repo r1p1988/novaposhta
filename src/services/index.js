af6e8ce143d44e3709da368b571e2f5ceae3fab8
FARTUSHNYI OLEKSII <102759585+r1p1988@users.noreply.github.com>
2023-03-23 13:56:56 +0200

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 export const API = `https://api.novaposhta.ua/v2.0/json/`;
-export const API_KEY = `834ddae50dcfc0cfb1b1382776cda647`;
+
 
 export const controller = async (url, method = `GET`, obj) => {
   let options = {
